d960a76983d1946b77458b6746ef3d85addd773e
SOL1archive <enlightkorean@gmail.com>
2023-04-08 17:39:33 +0900

    update:test code

--- test/db_connect.py
+++ test/db_connect.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     # DBConnect 객체 생성
     connect = DBConnect()
     for i in range(10):
-        connect.add(i, f'2021-01-{i:02}', f'product{i}', f'sub_title{i}', f'content{i}')
+        connect.add(i, f'2021-01-{i:02}', f'product{i}', f'sub_title{i}', f'content{i}', i)
     connect.commit()

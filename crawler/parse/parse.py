6a7df79d6ebed3d47c441cef811d8f01daab4a83
SOL1archive <enlightkorean@gmail.com>
2023-04-08 22:37:45 +0900

    update:parser & ftp

--- crawler/parse/parse.py
+++ crawler/parse/parse.py
@@ -3,51 +3,45 @@ import pathlib
 import ftplib
 
 import unit_split
+import strip
 import utils
 from db.clause import *
 
-# TODO: FTP, Local 파일 처리 일반화
-
 if __name__ == '__main__':
-    # DBConnect 객체 생성
     connect = DBConnect()
     yaml_path = pathlib.Path(__file__).parent.joinpath('parser-config.yaml')
     with open(yaml_path, 'r') as f:
         config = yaml.safe_load(f)
-    if config['Local']:
-        # 확인해야하는 디랙토리 리스트를 불러옴
-        csv_path = pathlib.Path(__file__).parent.joinpath('target_dirs.csv')
-    else:
-        # FTP 접속
-        ftp = ftplib.FTP(config['FTP']['host'], config['FTP']['user'], config['FTP']['password'])
-        ftp.cwd(config['FTP']['dir'])
-
+        
+    csv_path = pathlib.Path(__file__).parent.joinpath('target_dirs.csv')
     with open(csv_path, 'r') as f:
             target_dirs = [pathlib.Path(str_path) for str_path in f.readlines()]
+    
+    # FTP 접속
+    ftp = ftplib.FTP(config['FTP']['host'], config['FTP']['user'], config['FTP']['password'])
 
     # 각 파일에 대해 처리함
     for dir in target_dirs:
-        os.chdir(dir)
+        ftp.cwd(dir)
 
-        for file in os.listdir(dir):
+        for file in ftp.nlst():
             if utils.is_pdf(file):
                 pages = unit_split.get_pdf_pages(file)
                 page_units = unit_split.split_to_unit(pages)
 
                 # TODO: Erase Index Pages
+                clauses = strip.strip(page_units)
 
                 file_name_split = file[:-4].split('-')
                 ticker = file_name_split[0]
                 product = file_name_split[1]
                 doc_no = int(file_name_split[2])
+                for i, clause in enumerate(clauses):
+                    sub_title = '일반조항' if i == 0 else f'부칙{i}'
+                    contents = '\n'.join(clause)
+                    date = datetime.datetime.now().strftime('%Y-%m-%d')
 
-                # TODO date, sub_title, contents 추가
-                
-
-                connect.add()
-
-    if not config['Local']:
-        # FTP 접속 종료
-        ftp.quit()
-
+                    connect.add(ticker, date, product, sub_title, contents, doc_no)
+    
+    ftp.quit()
     connect.commit()

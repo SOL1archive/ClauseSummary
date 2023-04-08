09d32bec568db49f065518f2f549fe145e50d5b4
SOL1archive <enlightkorean@gmail.com>
2023-04-08 17:48:23 +0900

    update:db test string to datetime

--- db_connect.py
+++ db_connect.py
@@ -3,11 +3,12 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
 print(sys.path)
 
+import datetime
 from db.clause import *
 
 if __name__ == '__main__':
     # DBConnect 객체 생성
     connect = DBConnect()
     for i in range(10):
-        connect.add(i, f'2021-01-{i:02}', f'product{i}', f'sub_title{i}', f'content{i}', i)
+        connect.add(i, datetime.datetime(f'2021-01-{i:02}'), f'product{i}', f'sub_title{i}', f'content{i}', i)
     connect.commit()

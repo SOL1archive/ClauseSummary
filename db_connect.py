aafb41cc808ddbfbb5b9eac6b5458043b796ffba
HeIIowrld <121443413+HeIIowrld@users.noreply.github.com>
2023-04-11 01:00:27 +0900

    Update db_connect.py
    Fixed error input test (F-word)

--- db_connect.py
+++ db_connect.py
@@ -3,12 +3,18 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
 print(sys.path)
 
-import datetime
 from db.clause import *
 
 if __name__ == '__main__':
     # DBConnect 객체 생성
     connect = DBConnect()
-    for i in range(10):
-        connect.add(str(i), datetime.datetime(f'2021-01-{i:02}'), f'product{i}', f'sub_title{i}', f'content{i}', i)
+
+    for i in range(1,11):
+        connect.add('str(i)',\
+                f'2021-01-{i:02}',\
+                f'product{i}',\
+                f'sub_title{i}',\
+                f'content{i}',\
+                i)
+
     connect.commit()

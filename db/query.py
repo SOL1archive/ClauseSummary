0330a9cadda08c7c2c744aaee9f5de05ab265484
SOL1archive <enlightkorean@gmail.com>
2023-04-30 16:18:55 +0900

    update:queries

---
+++ db/query.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker
+
+from clause import Data, DBConnect
+
+# 전체 테이블 조회
+def select_all() -> pd.DataFrame:
+    pass
+
+# 특정 product 조회
+def product(product) -> pd.DataFrame:
+    pass

ecee0c90ecd807ca89880e5732e47454c8aebe9e
SOL1archive <enlightkorean@gmail.com>
2023-05-01 11:07:09 +0900

    update:parse add annotation

--- parse/unit_split.py
+++ parse/unit_split.py
@@ -12,18 +12,22 @@ def get_pdf_pages(file):
 # 목차 텍스트를 기준으로 데이터를 불러옴
 # 확인해야 할 점: 단순히 '목차'라는 단어가 나오는 경우에도 분리가 될 수 있음
 def split_to_unit(full_doc: list):
+    # 목차를 지정하는 키워드 저장
     with open('./crawler/parse/index.csv', 'r') as f:
         index_name_lt = []
         for line in f.readlines():
             index_name_lt.append(line[:-1])
     
+    # 목차가 나오는 페이지를 찾음
     index_pages = []
     for i, page in enumerate(full_doc):
         for index_name in index_name_lt:
-            if isin(page, index_name):
+            # 목차가 나오는 페이지를 찾음
+            if isin(page, index_name) or False: # TODO: 목차가 나오는 페이지를 찾는 방법을 개선해야 함
                 index_pages.append(i)
                 break
 
+    # 목차가 나오는 페이지를 기준으로 문서를 분리함
     index_pages.append(len(full_doc))
     result_doc = []
     for start, end in zip(index_pages, index_pages[1:]):

be21e0305fe4188030c605cddfe7ee564db16340
SOL1archive <enlightkorean@gmail.com>
2023-05-04 11:38:46 +0900

    test:init

--- test_code/parser_test.py
+++ test_code/parser_test.py
@@ -1,13 +1,14 @@
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
 import pytest
 
-from parse import parse_clauses, main
+from parse.parse import parse_clauses, main
 
 PRTNT_TEST = True
 
 file_list = [
-
+    './'
 ]
 
 @pytest.mark.parametrize('file', file_list)
@@ -15,6 +16,7 @@ def single_file_parse_test(file):
     ticker, date, product, sub_title, contents, doc_no = parse_clauses(file)
     if PRTNT_TEST:
         print(ticker, date, product, sub_title, contents, doc_no, sep='\n\n')
+    assert input(contents) == 'y'
     assert type(ticker) == str
     assert contents.count('\n') > 0
     assert contents.count('\n') < 10000

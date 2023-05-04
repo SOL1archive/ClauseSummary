881f3aed39bafbbaf2e20e9c0f4cc10fb1ce099a
SOL1archive <enlightkorean@gmail.com>
2023-05-04 10:58:32 +0900

    update:RLHF interface

--- RLHF/discord_bot.py
+++ RLHF/discord_bot.py
@@ -1,16 +1,44 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
+import yaml
+
 import discord
+
+from db.query import *
+
+with open('discord-token.yaml', 'r') as f:
+    token_data = yaml.safe_load(f)
+
+TOKEN = token_data['token']
+CHANNEL_ID = token_data['channel_id']
+
+class RLHFBot(discord.Client):
+    def is_score(self, message):
+        return (
+            message.content.isdigit() and
+            0 <= int(message.content) <= 10 and
+            message.channel.name == 'score' and
+            message.author != self.user and
+            message.author.name != 'RLHF'
+        )
+
+    async def on_ready(self):
+        await self.change_presence(
+            status=discord.Status.online, 
+            activity=discord.Game("점수 입력 대기중")
+        )
+    
+    # 메세지가 Score인지 체크하고 Score이면 DB에 저장함
+    async def on_message(self, message):
+        if self.is_score(message):
+            # TODO: DB에 저장 구현
+            pass
+            
+            # TODO: 다음 데이터를 출력
+            pass
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    pass
-
-@client.event
-async def on_message(message):
-    pass
-
+client = RLHFBot(intents=intents)
 client.run('your token here')
--- db/query.py
+++ db/query.py
@@ -12,3 +12,7 @@ def select_all() -> pd.DataFrame:
 # 특정 product 조회
 def product(product) -> pd.DataFrame:
     pass
+
+# 아직 라벨 되지 않은 데이터 조회 (한 건 씩 출력)
+def unlabeled() -> pd.DataFrame:
+    pass

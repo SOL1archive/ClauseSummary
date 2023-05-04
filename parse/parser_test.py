249bef89abbc1f1634db88605fc5d0fe5c97c4ac
SOL1archive <enlightkorean@gmail.com>
2023-05-04 23:00:24 +0900

    add:test code

--- parse/parser_test.py
+++ parse/parser_test.py
@@ -3,15 +3,17 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
 import pytest
 
-from parse.parse import parse_clauses, main
+from parse import parse_clauses, main
 
 PRTNT_TEST = True
 
 file_list = [
-    './'
+#    '001.pdf',
+#    '002.pdf',
+    '003.pdf',
 ]
 
-@pytest.mark.parametrize('file', file_list)
+#@pytest.mark.parametrize('file', file_list)
 def single_file_parse_test(file):
     ticker, date, product, sub_title, contents, doc_no = parse_clauses(file)
     if PRTNT_TEST:
@@ -20,3 +22,11 @@ def single_file_parse_test(file):
     assert type(ticker) == str
     assert contents.count('\n') > 0
     assert contents.count('\n') < 10000
+
+if __name__ == '__main__':
+    for file in file_list:
+        print(os.path.exists(file))
+
+    for file in file_list:
+        if os.path.exists(file):
+            single_file_parse_test(file)

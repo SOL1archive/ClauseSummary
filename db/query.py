44d8010b7d4ce86ccbc9add7882a8d48bfe72bbf
HeIIowrld <121443413+HeIIowrld@users.noreply.github.com>
2023-05-06 04:14:41 +0900

    Update query.py

--- db/query.py
+++ db/query.py
@@ -1,30 +1,31 @@
 import numpy as np
 import pandas as pd
 from sqlalchemy import select, Connection, MetaData, Table
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, 
 
 from clause import Data, DBConnect
 
 # 전체 테이블 조회 (학습용)
 def select_all() -> pd.DataFrame:
-    pass
+    ans = DBconnect.session.query(data).all()
+    return ans
 
 # 일정 수의 데이터 조회 (학습용)
 def select_n(n) -> pd.DataFrame:
-    pass
+    ans = DBconnect.session.query(data).limit(5).all()
+    return ans
 
 # 요약이 되지 않은 데이터 조회 (한 건 씩 출력)
-def summary_unlabeled() -> pd.DataFrame:
-    pass
+def summary_unlabeled(i) -> pd.DataFrame:
+    ans = DBconnect.session.query(data).filter_by(id=i).all()
+    return ans
 
 # 생성된 요약문을 DB에 저장
 def save_summary(row_no, summary) -> None:
-    pass
-
+    #add 함수 이용해야 할 듯?
+    
+    
 # Reward 라벨링이 되지 않은 데이터 조회 (한 건 씩 출력)
 def reward_unlabeled() -> pd.DataFrame:
-    pass
-
-# Human Feedback Reward를 DB에 저장
-def save_reward_label(row_no, reward) -> None:
-    pass
+    ans = DBconnect.session.query(sum_data).filter_by(id=i).all()
+    return ans

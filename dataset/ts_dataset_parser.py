65a1993b7ab5deaf6fcfd78e1c0984a9d8256b64
SOL1archive <enlightkorean@gmail.com>
2023-05-11 12:26:22 +0900

    update:ts_dataset

--- dataset/ts_dataset_parser.py
+++ dataset/ts_dataset_parser.py
@@ -14,10 +14,12 @@ def load_dataset_from_dir(path):
 
 # 전체 디렉토리를 파싱함
 def parse_dir(path):
+    path = pathlib.Path(path)
     pd_list = []
-    for path.child in path.iterdir():
-        if path.child.is_dir():
-            pd_list.append(parse_single_labeled_dir(path.child))
+    for path_child in path.iterdir():
+        if path_child.is_dir():
+            pd_list.append(parse_single_labeled_dir(path_child))
+        print(f'Parsed: {path_child.name}')
 
     return pd.concat(pd_list, axis=0)
 
@@ -37,9 +39,8 @@ def parse_single_labeled_dir(path):
             row['text'] = parse_file(child)
             row['title'] = child.name[child.name.find('_') + 1:child.name.rfind('_')]
             row['id'] = child.name[:child.name.find('_')]
+            row['label'] = label
             data.append(row)
-
-    data['label'] = [label] * len(path.iterdir())
 
     return pd.DataFrame(data)
 
@@ -47,9 +48,28 @@ def parse_single_labeled_dir(path):
 def parse_file(file):
     with open(file, 'r', encoding='utf-8') as f:
         soup = BeautifulSoup(f.read(), 'lxml')
-        text = (soup.select('cn').text
-                .replace('<![CDATA[', '')
-                .replace(']]>', '')
-                )
+        cn_lt = soup.find_all('cn')
+        text = ''
+        for cn in cn_lt:
+            text += (cn.text
+                    .replace('<![CDATA[', '')
+                    .replace(']]>', '')
+                    .strip()
+                    )
+            text += '\n'
     
     return text
+
+if __name__ == '__main__':
+    dir_lt = [
+        '/home/thesol1/datasets/clause-data/1.Training/terms',
+    ]
+    df_lt = []
+
+    for dir in dir_lt:
+        df_lt.append(
+            parse_dir(dir)
+        )
+
+    df = pd.concat(df_lt, axis=0)
+    df.to_pickle('data/dataset-term.pkl')

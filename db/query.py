47e8092811f3a9c7f560ca3e924a42f8a5edc75a
HeIIowrld <121443413+HeIIowrld@users.noreply.github.com>
2023-05-13 03:20:06 +0900

    Update query.py

--- db/query.py
+++ db/query.py
@@ -3,13 +3,14 @@ import pandas as pd
 from sqlalchemy import select, Connection, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 
+
 from clause import DBConnect, Data, Summary, Reward
 
 '''
 - db_connect 객체 입력받기
 - 문서 스펙에 맞춰서 데이터 조회
 - 전체 함수들 다 완성
-- **테스트**
+- 
 '''
 
 # 전체 테이블 조회 (학습용)
@@ -26,23 +27,24 @@ def select_n(db_connect,n):
 
 # 요약이 되지 않은 데이터 조회 (한 건 씩 출력)
 def summary_unlabeled(db_connect):
-    req = db_connect.session.query(Summary).filter(Summary.text is 'null').get(1)
-    ans = db_connect.session.query(Data).filter(Data.id == req).all()
-    df = pd.DataFrame(ans)
+    #ans = db_connect.session.query(Data).filter(Data.row_no == db_connect.session.query(Summary.row_no).filter(Summary.summary == None).first()).first()
+    ans = db_connect.session.query(Data.row_no).filter(Data.row_no == db_connect.session.query(Summary.row_no).filter(Summary.summary == None).first()).first()
+    df = pd.DataFrame(list(ans))
     return df
 
 # 생성된 요약문을 DB에 저장
-def save_summary(db_connect, row_no, summary):
-    db_connect.add_summary(row_no, summary)
-    db_connect.commit()
+def save_summary(row_no, summary):
+    sum_list = pd.values.tolist()
+    DBConnect.SummaryAdd(sum_list)
+    DBConnect.commit()
 
-def save_reward_label(db_connect, row_no, reward):
-    db_connect.add_reward(row_no, reward)
-    db_connect.commit()
+def save_reward_label(row_no, reward):
+    reward_list = pd.values.tolist()
+    DBConnect.RewardAdd(reward_list)
+    DBConnect.commit()
 
 # Reward 라벨링이 되지 않은 데이터 조회 (한 건 씩 출력)
 def reward_unlabeled(db_connect):
-    req = db_connect.session.query(Reward).filter(Reward.reward is 'null').get(1)
-    ans = db_connect.session.query(Data).join(Summary, Data.id == Summary.id).filter(Data.id == req).all()
+    ans = db_connect.session.query(Data).join(Summary, Data.row_no == Summary.row_no).outerjoin(Reward, Data.row_no == Reward.row_no).filter(Reward.reward != None).all()
     df = pd.DataFrame(ans)
     return df

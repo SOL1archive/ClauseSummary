f8d63d2fe4acd225191aac1f4526710dd81f2302
HeIIowrld <121443413+HeIIowrld@users.noreply.github.com>
2023-05-13 03:19:48 +0900

    Update clause.py

--- db/clause.py
+++ db/clause.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.dialects.mysql import TEXT, LONGTEXT
 
 # Mapping, 상속 클래스들을 자동으로 인지하고 매핑
 class Base(DeclarativeBase):
@@ -15,67 +16,60 @@ class Base(DeclarativeBase):
         return f"{self.__class__.__name__}({', '.join(f'{k}={v}' for k, v in self.__dict__.items())})"
 
 class Data(Base):
-    __tablename__ = 'data'  # data 테이블과 매핑된다.
-    ticker = mapped_column(String, nullable=False)
-    date = mapped_column(String, nullable=False)
-    product = mapped_column(String, nullable=False)
-    sub_title = mapped_column(String, nullable=False)
-    content = mapped_column(String, nullable=False)
-    doc_no = mapped_column(String, nullable=False)
+    __tablename__ = 'data'  # summary 테이블과 매핑된다.
     row_no = mapped_column(Integer, nullable=False, primary_key=True)
+    text = mapped_column(LONGTEXT, nullable=False)
+    title = mapped_column(LONGTEXT, nullable=False)
+    id = mapped_column(Integer, nullable=False)
+    label = mapped_column(TEXT)
     
-    def __init__(self, ticker, date, product, sub_title, content, doc_no):
-        self.ticker = ticker
-        self.date = date
-        self.product = product
-        self.sub_title = sub_title
-        self.content = content
-        self.doc_no = doc_no
-    #    self.row_no = row_no
-    #   row_no is annotated due to SQL server side auto-increment setting, so don't un-annotate.
-    
-   # def __repr__(self):
-   #     return 'user_id : %s, user_name : %s, profile_url : %s' % (self.user_id, self.user_name, self.profile_url)
+    def __init__(self, row_no, text, title, id, label):
+        self.row_no = [row_no]
+        self.text = [text]
+        self.title = [title]
+        self.id = [id]
+        self.label = [label]
+
     
 class Summary(Base):
     __tablename__ = 'summary'  # summary 테이블과 매핑된다.
-    id = mapped_column(Integer, nullable=False, primary_key=True)
-    summary = mapped_column('longtext', nullable=False)
+    row_no = mapped_column(Integer, nullable=False, primary_key=True)
+    summary = mapped_column(LONGTEXT, nullable=False)
     
-    def __init__(self, id, summary):
-        self.id = id
-        self.summary = summary
+    def __init__(self, row_no, summary):
+        self.row_no = [row_no]
+        self.summary = [summary]
 
 class Reward(Base):
     __tablename__ = 'reward'  # reward 테이블과 매핑된다.
-    id = mapped_column(Integer, nullable=False, primary_key=True)
+    row_no = mapped_column(Integer, nullable=False, primary_key=True)
     reward = mapped_column(Integer)
     
-    def __init__(self, id, reward):
-        self.id = id
-        self.reward = reward
+    def __init__(self, row_no, reward):
+        self.row_no = [row_no]
+        self.reward = [reward]
         
 class DBConnect:
     def __init__(self) -> None:
         db_config_path = pathlib.Path(__file__).parent.joinpath('db.yaml')
         with open(db_config_path, "r") as f:
             credentials = yaml.safe_load(f)
-        self.db_url = f"mysql+pymysql://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{credentials['database']}"
+        self.db_url = 'mysql+pymysql://tosan:tosan@localhost/tosan'
         self.engine = create_engine(self.db_url)
 
         # engine 종속적 session 정의
         self.Session = sessionmaker(self.engine)
         self.session = self.Session()
     
-    def add_data(self, *argv, **kwarg):
+    def DataAdd(self, *argv, **kwarg):
         row = Data(*argv, **kwarg)
         self.session.add(row)
         
-    def add_summary(self, *argv, **kwarg):
-        row = Reward(*argv, **kwarg)
+    def SummaryAdd(self, *argv, **kwarg):
+        row = Summary(*argv, **kwarg)
         self.session.add(row)
 
-    def add_reward(self, *argv, **kwarg):
+    def RewardAdd(self, *argv, **kwarg):
         row = Reward(*argv, **kwarg)
         self.session.add(row)
 

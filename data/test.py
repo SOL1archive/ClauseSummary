934375e2f89c40b076476c40eec1c162438b1276
HeIIowrld <121443413+HeIIowrld@users.noreply.github.com>
2023-05-13 03:37:24 +0900

    Update test.py

--- data/test.py
+++ data/test.py
@@ -11,6 +11,11 @@ import pymysql
 import query
 import clause
 
+
+    sql = text('select * from data where data.row_no = (select summary.row_no from summary where summary.summary is null limit 1)')
+    ans = db_connect.session.execute(sql)
+    df = pd.DataFrame(list(ans))
+
 db_connect = clause.DBConnect()
 
 #df.to_sql(name='labeled_pretrained', con=db_connection, if_exists='append')

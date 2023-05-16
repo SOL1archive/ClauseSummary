3698fe2fb3681b8191edc7398e333665f29cf729
SOL1archive <enlightkorean@gmail.com>
2023-05-16 19:34:05 +0900

    update:preprocessing test part

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -2,11 +2,14 @@ import re
 import numpy as np
 import pandas as pd
 
-df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
 def summary_preprocessing_func1(text:str) -> str: #숫자. 형태로 되어있는것에 개행문자를 추가.
     pattern = r'(\d+)\.'
     return re.sub(pattern,r'\n\1', str(text))
-result = summary_preprocessing_func1(df)
 
-print(df['summary'][0])
-df.head()
+if __name__ == '__main__':
+    df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
+    text = df['summary'][0]
+    result = summary_preprocessing_func1(text)
+
+    print(text, result, sep='\n' + '-'*100 + '\n')
+    

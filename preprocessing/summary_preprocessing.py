c0341c71a36030ae6bd59dec344b2669c85dac5e
cwohk1 <89119052+cwohk1@users.noreply.github.com>
2023-05-17 06:57:24 +0900

    Update summary_preprocessing.py

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -6,6 +6,13 @@ def summary_preprocessing_func1(text:str) -> str: #숫자. 형태로 되어있�
     pattern = r'(\d+)\.'
     return re.sub(pattern,r'\n\1', str(text))
 
+def summary_preprocessing_func(text:str): # (1) (2) 형태를 ,으로
+    items = re.split(r'\(\d+\)', text)
+    if len(items) > 1:
+        items[1] = items[0]+items[1]
+        del items[0]
+    return ','.join(items)
+
 if __name__ == '__main__':
     df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
     text = df['summary'][0]

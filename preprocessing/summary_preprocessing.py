887da908362f5945a784699aae2d084a13f9acf5
SOL1archive <enlightkorean@gmail.com>
2023-05-18 00:47:54 +0900

    update:preprocessing

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -13,25 +13,20 @@ def summary_preprocessing_func2(text:str): # (1) (2) 형태를 ,으로
         del items[0]
     return ','.join(items)
 
-<<<<<<< Updated upstream
+def summary_preprocessing_func3(text:str) -> str:
+    text = re.sub(r"\b갑\b", r'갑\b', text)
+    text = re.sub(r"\b을\b", r'을\b', text)
+    text = re.sub(r"\b병\b", r'정\b', text)
+    text = re.sub(r"\b병\b", r'정\b', text)
+
+    return text
+
 def summary_preprocessing_func(text: str):
     text = summary_preprocessing_func1(text)
     text = summary_preprocessing_func2(text)
+    text = summary_preprocessing_func3(text)
     
     return text
-=======
-def summary_preprocessing_func2(text:str) -> str:
-    pattern = r'\b?갑?\b|\b?을?\b|\?b병?\b|\b?정?\b'
-    if pattern == "\b갑\b":
-        return re.sub(pattern, r'갑\b', str)
-    elif pattern == "\b을\b":
-        return re.sub(pattern, r'을\b', str)
-    elif pattern == "\b병\b":
-        return re.sub(pattern, r'정\b', str)
-    elif pattern == "\b병\b":
-        return re.sub(pattern, r'정\b', str)
->>>>>>> Stashed changes
-
 if __name__ == '__main__':
     df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
     text = df['summary'][0]

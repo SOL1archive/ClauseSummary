7e0819e3a5447d5b5677b297e46791659f704f63
BOBkumo <leeseungyun123@gmail.com>
2023-05-18 00:41:28 +0900

    update summary_preprocessing

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -13,16 +13,29 @@ def summary_preprocessing_func2(text:str): # (1) (2) 형태를 ,으로
         del items[0]
     return ','.join(items)
 
+<<<<<<< Updated upstream
 def summary_preprocessing_func(text: str):
     text = summary_preprocessing_func1(text)
     text = summary_preprocessing_func2(text)
     
     return text
+=======
+def summary_preprocessing_func2(text:str) -> str:
+    pattern = r'\b?갑?\b|\b?을?\b|\?b병?\b|\b?정?\b'
+    if pattern == "\b갑\b":
+        return re.sub(pattern, r'갑\b', str)
+    elif pattern == "\b을\b":
+        return re.sub(pattern, r'을\b', str)
+    elif pattern == "\b병\b":
+        return re.sub(pattern, r'정\b', str)
+    elif pattern == "\b병\b":
+        return re.sub(pattern, r'정\b', str)
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
     text = df['summary'][0]
-    result = summary_preprocessing_func1(text)
+    result = summary_preprocessing_func2(text)
 
     print(text, result, sep='\n' + '-'*150 + '\n')
     
--- preprocessing/testsat.py
+++ preprocessing/testsat.py
@@ -1,10 +1,10 @@
 import re
 
 def remove_number_dot_from_string(str):
-    pattern = r'(\d+)\.'
-    return re.sub(pattern, r'\n\1.', str)
+    pattern = r'\b을\b'
+    return re.sub(pattern, r'', str)
 
 # 예시
-string_with_number_dot = "Hello123. World456.!"
+string_with_number_dot = "을오징어을 오징어 을 오징어"
 result = remove_number_dot_from_string(string_with_number_dot)
 print(result)

2d7d665feffec62ac8c7068cb45c56779cb8dd69
SOL1archive <enlightkorean@gmail.com>
2023-05-17 22:42:24 +0900

    update:summary preprocessing

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -6,12 +6,18 @@ def summary_preprocessing_func1(text:str) -> str: #숫자. 형태로 되어있�
     pattern = r'(\d+)\.'
     return re.sub(pattern,r'\n\1.', str(text))
 
-def summary_preprocessing_func(text:str): # (1) (2) 형태를 ,으로
+def summary_preprocessing_func2(text:str): # (1) (2) 형태를 ,으로
     items = re.split(r'\(\d+\)', text)
     if len(items) > 1:
         items[1] = items[0]+items[1]
         del items[0]
     return ','.join(items)
+
+def summary_preprocessing_func(text: str):
+    text = summary_preprocessing_func1(text)
+    text = summary_preprocessing_func2(text)
+    
+    return text
 
 if __name__ == '__main__':
     df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')

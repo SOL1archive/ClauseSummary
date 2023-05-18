33fae24cd826ce51442f1673a0c4b23290443068
SOL1archive <enlightkorean@gmail.com>
2023-05-18 20:21:56 +0900

    update:test view

--- preprocessing/text_preprocessing.py
+++ preprocessing/text_preprocessing.py
@@ -1,3 +1,13 @@
 import re
+import pandas as pd
+
 def text_preprocessing_func(text):
     return re.sub(r'\n[\n ]+', '\n', text)
+
+if __name__ == '__main__':
+    df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
+    text = df['text'][0]
+    result = text_preprocessing_func(text)
+
+    print(text, result, sep='\n' + '-'*150 + '\n')
+    

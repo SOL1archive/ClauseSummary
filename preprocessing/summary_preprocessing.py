0e2399a6f104024e3892ceaaa57ea2c691cbd6e0
cwohk1 <89119052+cwohk1@users.noreply.github.com>
2023-05-19 14:39:20 +0900

    Update summary_preprocessing.py

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -25,32 +25,12 @@ def summary_preprocessing_func(text: str):
     text = summary_preprocessing_func2(text)
     text = summary_preprocessing_func3(text)
     
-def summary_preprocessing_func4(text:str): # 1. 2. 등을 제 1조 2 항 등으로 바꿔줌
-    p_r = re.finditer(r"제 *\d조", str(text))
-    paragraphs = [m.span() for m in p_r]#[:][1] # end index of each paragraph
-    #print(paragraphs[0])
-    if len(paragraphs) == 0:
-        return text
-    print("Paragraphs idex : ", paragraphs)
-    paragraphs = [p[1] for p in paragraphs]
-    paragraph_nums = [m.match() for m in p_r]
-
-    a_r = re.finditer(r"\d+\.", str(text))
-    articles = [m.span() for m in a_r]
-    print("article length", len(articles))
-    article_nums = [int(re.search(r"\d", s.match())) for s in a_r]
-    #s = re.split(r"[0-9]+\.", str(text))
-    paragraph_idx = 0
-    for i in range(len(articles)):
-        while paragraph_idx < len(paragraphs) and articles[i][0] > paragraphs[paragraph_idx]:
-            paragraph_idx += 1
-        paragraph_idx -= 1
-        if paragraph_idx < 0:
-            continue
-        replaced = "제 %s조 %d항"%(paragraph_nums[paragraph_idx], article_nums[i])
-        print(replaced)
-        text = text[:articles[i][0]] + replaced + text[articles[i][1]:]
-    return text
+  def summary_preprocessing_func4(text): #1. 2. 등을 지운다.
+    items = re.split(r'\d+\.', text)
+    if len(items) > 1:
+        items[1] = items[0]+items[1]
+        del items[0]
+    return ''.join(items)
 
 if __name__ == '__main__':
     df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')

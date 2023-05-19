3fe3eb5d5a0efd4ddf06db8c4c6c662a2a9d1d0d
BOBkumo <leeseungyun123@gmail.com>
2023-05-19 18:24:21 +0900

    update_summary_preprocessing

--- preprocessing/summary_preprocessing.py
+++ preprocessing/summary_preprocessing.py
@@ -2,25 +2,25 @@ import re
 import numpy as np
 import pandas as pd
 
-def summary_preprocessing_func1(text:str) -> str: #숫자. 형태로 되어있는것에 개행문자를 추가.
+def add_newline_before_number(text:str) -> str: #숫자. 형태로 되어있는것에 개행문자를 추가.
     text = re.sub(r'(\d+)\.',r'\n\1.', str(text))
     return text
 
-def summary_preprocessing_func2(text:str): # (1) (2) 형태를 ,으로
+def change_it_to_a_comma(text:str): # (1) (2) 형태를 ,으로
     items = re.split(r'\(\d+\)', text)
     if len(items) > 1:
         items[1] = items[0]+items[1]
         del items[0]
     return ','.join(items)
 
-def summary_preprocessing_func3(text:str) -> str:
-    text = re.sub(r"\b갑\b", r'갑\b', str(text))
-    text = re.sub(r"\b을\b", r'을\b', str(text))
-    text = re.sub(r"\b병\b", r'병\b', str(text))
-    text = re.sub(r"\b정\b", r'정\b', str(text))
-    return text
+def remove_whitespace_after_str(text:str):
+        text = re.sub(r"\b갑\b", r'갑\b', text)
+        text = re.sub(r"\b을\b", r'을\b', text)
+        text = re.sub(r"\b병\b", r'병\b', text)
+        text = re.sub(r"\b정\b", r'정\b', text)
+        return text
 
-def summary_preprocessing_func4(text): #1. 2. 등을 지운다.
+def erase_the_number_point(text): #1. 2. 등을 지운다.
     items = re.split(r'\d+\.', text)
     if len(items) > 1:
         items[1] = items[0]+items[1]
@@ -28,15 +28,14 @@ def summary_preprocessing_func4(text): #1. 2. 등을 지운다.
     return ''.join(items)
 
 def summary_preprocessing_func(text: str):
-    text = summary_preprocessing_func1(text)
-    text = summary_preprocessing_func2(text)
-    text = summary_preprocessing_func3(text)
-    text = summary_preprocessing_func4(text)
-    return text
+    text = add_newline_before_number(text)
+    text = change_it_to_a_comma(text)
+    text = remove_whitespace_after_str(text)
+    text = erase_the_number_point(text)
 
 if __name__ == '__main__':
     df = pd.read_json('./data/dataset-term-summary.json', encoding='utf-8')
     text = df['summary'][0]
-    result = summary_preprocessing_func3(text)
+    result = remove_whitespace_after_str(text)
 
     print(text, result, sep='\n' + '-'*150 + '\n')

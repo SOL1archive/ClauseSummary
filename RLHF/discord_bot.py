076048968a19cbc02f5ea8720abf744bc7d479b9
BOBkumo <leeseungyun123@gmail.com>
2023-05-25 05:09:24 +0900

    update_discord_bot

--- RLHF/discord_bot.py
+++ RLHF/discord_bot.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
 import yaml
-
+import mysql.connector
 import discord
 
 from db.query import *
@@ -10,8 +10,21 @@ from db.query import *
 with open('discord-token.yaml', 'r') as f:
     token_data = yaml.safe_load(f)
 
-TOKEN = token_data['token']
-CHANNEL_ID = token_data['channel_id']
+conn = mysql.connector.connect(
+    host = '182.216.63.62',
+    user = 'tosan',
+    password = 'tosan',
+    database = 'tosan'
+)
+consor = conn.cursor()
+
+#수정필요
+creat_table_query = '''
+
+'''
+
+TOKEN = token_data['MTEwNzI0NzA4MDk1Njc2MDA4NA.GqLmxP.wYDgszT4OW9ImGStviq1mLPLjtg2qa7ZqGePFg']
+CHANNEL_ID = token_data['1107264599025274991']
 
 class RLHFBot(discord.Client):
     def is_score(self, message):
@@ -32,9 +45,21 @@ class RLHFBot(discord.Client):
     # 메세지가 Score인지 체크하고 Score이면 DB에 저장함
     async def on_message(self, message):
         if self.is_score(message):
-            # TODO: DB에 저장 구현
-            pass
+            # DB에 저장 구현
+                # message 추출
+            content = message.contentdiscord.py
+            author_id = str(message.author.id)
+            channel_name = message.channel.name
+            timestamp = message.created_at
             
+            #수정필요
+            inset_query = '''
+            content, author_id, channel_name, timestamp
+            '''
+            values = (content, author_id, channel_name, timestamp)
+
+            conn.commit()
+
             # TODO: 다음 데이터를 출력
             pass
 
@@ -42,3 +67,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = RLHFBot(intents=intents)
 client.run(TOKEN)
+conn.close()

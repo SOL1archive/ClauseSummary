49db4abde131c0c08a23627fc2f1ac50f05e64b8
BOBkumo <leeseungyun123@gmail.com>
2023-05-25 18:47:40 +0900

    update_discord_bot

--- RLHF/discord_bot.py
+++ RLHF/discord_bot.py
@@ -19,9 +19,9 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 #수정필요
-creat_table_query = '''
+#creat_table_query = '''
 
-'''
+#'''
 
 TOKEN = token_data['your token']
 CHANNEL_ID = token_data['channel ID']
@@ -41,16 +41,21 @@ class RLHFBot(discord.Client):
             status=discord.Status.online, 
             activity=discord.Game("점수 입력 대기중")
         )
-    
+
     # 메세지가 Score인지 체크하고 Score이면 DB에 저장함
     async def on_message(self, message):
         if self.is_score(message):
+            
+            #한줄씩 출력
+            cursor.execute("SELECT * FROM dataset")
+            data = cursor.fetchall()
             # DB에 저장 구현
+            for row_no in data:
                 # message 추출
-            content = message.contentdiscord.py
-            author_id = str(message.author.id)
-            channel_name = message.channel.name
-            timestamp = message.created_at
+                content = message.contentdiscord.py
+                author_id = str(message.author.id)
+                channel_name = message.channel.name
+                timestamp = message.created_at
             
             #수정필요
             inset_query = '''

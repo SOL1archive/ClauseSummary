fd49cc7b5c4ff22bd17dce65e2c4b9ff94cf8042
BOBkumo <leeseungyun123@gmail.com>
2023-05-27 22:57:18 +0900

    update_discord_bot

--- RLHF/discord_bot.py
+++ RLHF/discord_bot.py
@@ -6,24 +6,27 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
 import yaml
 #import mysql.connector
 import discord
+from discord.ext import commands
 
 import db.clause as clause
 import db.query as query
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 with open('/home/bob/바탕화면/ClauseSummary/RLHF/discord-token.yaml', 'r') as f:
     token_data = yaml.safe_load(f)
-
-#수정필요
-#creat_table_query = '''
-
-#'''
 
 TOKEN = token_data['token']
 CHANNEL_ID = token_data['channel ID']
 
-class RLHFBot(discord.Client):
+class RLHFBot(commands.Cog):
     def __init__(self, **options: Any) -> None:
-        super().__init__(**options)
+        super().__init__(
+            command_prefix='$',
+            intents = intents.all(),
+            sync_command = True,
+            **options)
         self.db_connect = clause.DBConnect()
         # 첫번째 출력
 
@@ -41,6 +44,9 @@ class RLHFBot(discord.Client):
             status=discord.Status.online, 
             activity=discord.Game("점수 입력 대기중")
         )
+        data = query.reward_unlabeled(self.db_connect)
+        self.row_no = data['row_no']
+        print(self.row_no)
 
     # 메세지가 Score인지 체크하고 Score이면 DB에 저장함
     # TODO: 
@@ -56,15 +62,9 @@ class RLHFBot(discord.Client):
             # DB에 저장 
             for row_no in data:
                 # message 추출
-                content = message.contentdiscord.py
-                author_id = str(message.author.id)
-                channel_name = message.channel.name
-                timestamp = message.created_at
             
             #수정필요
-            values = (content, author_id, channel_name, timestamp)
-
-            self.db_connect.commit()
+                self.db_connect.commit()
 
             # TODO: 다음 데이터를 출력
             pass
@@ -72,7 +72,5 @@ class RLHFBot(discord.Client):
         def __del__(self):
             self.db_connect.close()
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = RLHFBot(intents=intents)
-client.run(TOKEN)
+bot = RLHFBot()
+bot.run(TOKEN)

86266adbac3d4f78236b22385067952c1404bd2a
BOBkumo <leeseungyun123@gmail.com>
2023-05-27 18:41:21 +0900

    update_discord_bot2

--- RLHF/discord_bot2.py
+++ RLHF/discord_bot2.py
@@ -1,49 +1,69 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('./..'))))
+from discord.flags import Intents
+from typing import Any
 import yaml
-import pymysql
 import discord
+import db.clause as clause
+import db.query as query
 
-#from db.query import *
+from discord.ext import commands
 
-#with open('discord-token.yaml', 'r') as f:
- #   token_data = yaml.safe_load(f)
-
-conn = pymysql.connect(
-    host = '182.216.63.62',
-    user = 'tosan',
-    password = 'tosan',
-    database = 'tosan'
-)
-cursor = conn.cursor(pymysql.cursors.DictCursor)
+intents = discord.Intents.default()
+intents.message_content = True
 
 
-TOKEN = 'MTEwNzI0NzA4MDk1Njc2MDA4NA.GlVOi7.QjwocNrczvCDPfNWz2jGz3v6lRdq-OLJGoccwo'
-CHANNEL_ID = '1107264599025274991'
+with open('discord-token.yaml', 'r') as f:
+    token_data = yaml.safe_load(f)
 
-class RLHFBot(discord.Client):
+TOKEN = token_data['token']
+CHANNEL_ID = token_data['channel ID']
 
+class RLHFBot(discord.ext.commands.Bot):
+    def __init__(self, **options: Any) -> None:
+        super().__init__(
+            command_prefix='$',
+            intents = intents.all(),
+            sync_command = True,
+            **options)
+        self.db_connect = clause.DBConnect()
+
+    def is_score(self, message):
+        return (
+            message.content.isdigit() and
+            0 <= int(message.content) <= 10 and
+            message.channel.name == 'score' and
+            message.author != self.user and
+            message.author.name != 'RLHF'
+        )
+    
     async def on_ready(self):
+        print(f"We have logged in as {bot.user}")
         await self.change_presence(
             status=discord.Status.online, 
-            activity=discord.Game("점수 입력 대기중")
+            activity=discord.Game("'$'를 사용하여 시작해보세요!")
         )
+    
+    #잘못입력된 메세지 입력 시 반환
+    async def on_command_error(message, error):
+        if isinstance(error, commands.CommandNotFound):
+            await message.send("명령어를 찾지 못했습니다.")
 
     # 메세지가 Score인지 체크하고 Score이면 DB에 저장함
+
     async def on_message(self, message):
         if self.is_score(message):
             
             #한줄씩 출력
-            cursor.execute("SELECT * FROM dataset")
-            data = cursor.fetchone()
+            data = query.reward_unlabeled(self.db_connect)
+            self.row_no = data['row_no']
             for row_no in data:
                 print(data)
-            
-            
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = RLHFBot(intents=intents)
-client.run(TOKEN)
-conn.close()
+    #def __del__(self):
+    #        self.db_connect = clause.DBConnect()
+    #        self.db_connect.mysql_close()
+
+bot = RLHFBot()
+bot.run(TOKEN)

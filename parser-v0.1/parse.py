d9904ce76f0a87f7eb55dbc86e6723c9283bcd76
HeIIowrld <121443413+HeIIowrld@users.noreply.github.com>
2023-06-29 01:57:49 +0900

    Update parse.py

--- parser-v0.1/parse.py
+++ parser-v0.1/parse.py
@@ -14,19 +14,22 @@ import db.clause as clause
 # 문서 하나를 파싱함
 def parse_clauses(file):
     pages = unit_split.get_pdf_pages(file)
-    page_units = unit_split.split_to_unit(pages)
-    clauses = cs.articles2clauses(page_units)
+    #page_units = unit_split.split_to_unit(pages)
+    clauses = '\n\n'.join(pages)
     
     file_name_split = os.path.basename(file)[:-4].split('-')
     label = file_name_split[0]
     title = file_name_split[1]
     id = int(file_name_split[2])
-
+    sub_title = 'null'
+    '''
     for i, clause in enumerate(clauses):
         sub_title = '일반조항' if i == 0 else f'부칙{i}'
         text = '\n'.join(clause)
 
         yield (text, title, sub_title, id, label)
+    '''
+    return (clauses, title, sub_title, id, label)
 
 def main_local():
     connect = clause.DBConnect()
@@ -42,11 +45,10 @@ def main_local():
     for dir in target_dirs:
         for file in dir.iterdir():
             if utils.is_pdf(file):
-                for (text, title, sub_title, id, label) in parse_clauses(file):
-                    print(text, title, sub_title, id, label)
-                    connect.add_data(text, title, sub_title, id, label)
-    
-    connect.commit()
+                text, title, sub_title, id, label = parse_clauses(file)
+                print(text, title, sub_title, id, label)
+                connect.add_data(text, title, sub_title, id, label)
+                connect.commit()
 
 def main_ftp():
     connect = clause.DBConnect()

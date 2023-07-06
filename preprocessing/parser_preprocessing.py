dde3a76f659a98ba9516ef31ad5fdcad0478c7d1
BOBkumo <leeseungyun123@gmail.com>
2023-07-07 00:46:50 +0900

    update_paser_preprocessing

--- preprocessing/parser_preprocessing.py
+++ preprocessing/parser_preprocessing.py
@@ -1,12 +1,31 @@
-def remove_duplicated_chars(characters): 
+import re
+import pandas as pd
+
+
+def remove_duplicated_chars(text:str): 
     result = "" 
-    result += characters[0]
-    for i in range(1, len(characters)): 
-        if characters[i - 1] != characters[i]: 
-            result += characters[i] 
+    result += text[0]
+    for i in range(1, len(text)): 
+        if text[i - 1] != text[i]: 
+            result += text[i] 
     
     return result 
 
+def remove_special_characters(text:str):
+    text = re.sub(r"·+",r" ", str(text))
+
+    return text
+
+def remove_new_line_character(text:str):
+    #text = re.sub(r"\.(?:\n)+", '.', str(text))
+    text = re.sub(r"\.(.*?)(\.\n)", r" ", text)
+
+    return text
+
 if __name__ == "__main__":
-    characters = "senteeeencccccceeee" 
-    ret = remove_duplicated_chars(characters)
+    text = "senteeeencccccceeee··············.\n\ndkfahksdglknasdkg.\n\n\n.dalkg\ndlfahadsf\n." 
+    ret = remove_duplicated_chars(text)
+    ret2 = remove_special_characters(text)
+    ret3 = remove_new_line_character(text)
+    print(ret3)
+    print(ret2)

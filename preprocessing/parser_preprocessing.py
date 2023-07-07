b04c41c5c0302531679df04704ddc575dad3c924
BOBkumo <leeseungyun123@gmail.com>
2023-07-08 01:34:14 +0900

    update_parser_preprocessing

--- preprocessing/parser_preprocessing.py
+++ preprocessing/parser_preprocessing.py
@@ -22,6 +22,13 @@ def remove_new_line_character(text:str):
 
     return text
 
+def remove_number_special_characters(text:str):
+    text_list = ['\nU+2460', '\nU+2461', '\nU+2462', '\nU+2463', '\nU+2464', '\nU+2465', '\nU+2466', '\nU+2467', '\nU+2468', '\nU+2469', '\nU+246A', '\nU+246B', '\nU+246C', '\nU+246D', '\nU+246E', '\nU+246F', '\nU+2470', '\nU+2471', '\nU+2472', '\nU+2473', '\nU+3251', '\nU+3252', '\nU+3253', '\nU+3254', '\nU+3255', '\nU+3256', '\nU+3257', '\nU+3258', '\nU+3259', '\nU+325A', '\nU+325B', '\nU+325C', '\nU+325D', '\nU+325E', '\nU+325F', '\nU+32B1', '\nU+32B2', '\nU+32B3', '\nU+32B4', '\nU+32B5', '\nU+32B6', '\nU+32B7', '\nU+32B8', '\nU+32B9', '\nU+32BA', '\nU+32BB', '\nU+32BC', '\nU+32BD', '\nU+32BE', '\nU+32BE',]
+    text_pattern = '|'.join(re.escape(char) for char in text_list)
+    text = re.sub(text_pattern, "", str(text))
+
+    return text
+
 if __name__ == "__main__":
     text = "senteeeencccccceeee··············.\n\ndkfahksdglknasdkg.\n\n\n.dalkg\ndlfahadsf.\n안녕.\nㅏ안녕안녕\n\n안녕.\n\n" 
     ret = remove_duplicated_chars(text)
